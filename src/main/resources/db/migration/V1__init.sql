0b1367b1ef848c1c16f6efdf2ad06eac7b338868
mauricio <mauriciovilela@gmail.com>
2018-12-15 21:38:22 -0200

    beestore actuator

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE PERSON (
+	id BIGINT,
+	first_name varchar(255) not null,
+	last_name varchar(255) not null
+);
+
+insert into PERSON (id, first_name, last_name) values (1, 'Dave', 'Syer');
